fe6575690baaff74ba58ef30690edd69ec045225
Marco Petraccini <129108720+Petraccino@users.noreply.github.com>
2025-04-01 09:42:35 +0200

    Project hr-payroll WIP

---
+++ hr-payroll/src/main/java/it/petraccino/hrpayroll/feignclient/WorkerClient.java
@@ -0,0 +1,15 @@
+package it.petraccino.hrpayroll.feignclient;
+
+import it.petraccino.hrpayroll.entity.Worker;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "hr-worker", url = "localhost:8001", path = "/workers")
+public interface WorkerClient {
+
+    @GetMapping(value = "/{id}")
+    ResponseEntity<Worker> findById(@PathVariable Long id);
+
+}
--- hr-payroll/src/main/java/it/petraccino/hrpayroll/service/PayrollService.java
+++ hr-payroll/src/main/java/it/petraccino/hrpayroll/service/PayrollService.java
@@ -2,26 +2,18 @@ package it.petraccino.hrpayroll.service;
 
 import it.petraccino.hrpayroll.entity.Payment;
 import it.petraccino.hrpayroll.entity.Worker;
+import it.petraccino.hrpayroll.feignclient.WorkerClient;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class PayrollService {
 
-    @Value("${hr-worker.host}")
-    private String host;
-    private final RestTemplate restTemplate;
+    private final WorkerClient workerClient;
 
     public Payment getPayment(long workerId, int days){
-        Map<String, String> uriVariable = new HashMap<>();
-        uriVariable.put("id", ""+workerId);
-        Worker worker = restTemplate.getForObject(host+"/workers/{id}", Worker.class, uriVariable);
+        Worker worker = workerClient.findById(workerId).getBody();
         return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 
