c6b7f347a0aff5396f02c7f9699a4951502cfe93
Marco Petraccini <129108720+Petraccino@users.noreply.github.com>
2025-04-01 09:25:51 +0200

    Project hr-payroll WIP

--- hr-payroll/src/main/java/it/petraccino/hrpayroll/service/PayrollService.java
+++ hr-payroll/src/main/java/it/petraccino/hrpayroll/service/PayrollService.java
@@ -1,15 +1,28 @@
 package it.petraccino.hrpayroll.service;
 
 import it.petraccino.hrpayroll.entity.Payment;
+import it.petraccino.hrpayroll.entity.Worker;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class PayrollService {
 
+    @Value("${hr-worker.host}")
+    private String host;
+    private final RestTemplate restTemplate;
+
     public Payment getPayment(long workerId, int days){
-        return new Payment("Bob", 200.0, days);
+        Map<String, String> uriVariable = new HashMap<>();
+        uriVariable.put("id", ""+workerId);
+        Worker worker = restTemplate.getForObject(host+"/workers/{id}", Worker.class, uriVariable);
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 
 }
