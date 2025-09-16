64e3d913d10e9a9f518dfe59e1cad3574b2e1776
Marco Petraccini <129108720+Petraccino@users.noreply.github.com>
2025-09-16 10:10:02 +0200

    Add OAuth2 resource server security configuration
    Introduces SecurityConfig to enable JWT-based authentication using spring-boot-starter-oauth2-resource-server. Updates pom.xml to include the necessary dependency and configures public access for actuator and Swagger endpoints.

---
+++ hr-api-gateway/src/main/java/it/petraccino/hrapigateway/authorization/SecurityConfig.java
@@ -0,0 +1,28 @@
+package it.petraccino.hrapigateway.authorization;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(ex -> ex
+                        .pathMatchers("/actuator/**").permitAll()
+                        .pathMatchers("/v3/api-docs/**","/swagger-ui/**").permitAll()
+                        .anyExchange().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> {
+                }))
+                .build();
+    }
+}
+
+
