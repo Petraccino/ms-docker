ae2ad5bb06e37fa83ddb5f6ccffe88a838547614
Marco Petraccini <129108720+Petraccino@users.noreply.github.com>
2025-10-06 17:21:53 +0200

    Relay token qith feign client

--- hr-payroll/src/main/java/it/petraccino/hrpayroll/authorization/SecurityConfig.java
+++ hr-payroll/src/main/java/it/petraccino/hrpayroll/authorization/SecurityConfig.java
@@ -1,13 +1,18 @@
 package it.petraccino.hrpayroll.authorization;
 
 
+import feign.RequestInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.util.StringUtils;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 @Configuration
 @EnableWebSecurity
@@ -17,12 +22,26 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/actuator/**","/v3/api-docs/**","/swagger-ui/**").permitAll()
+                        .requestMatchers("/actuator/**", "/v3/api-docs/**", "/swagger-ui/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
         return http.build();
     }
+
+    @Bean
+    RequestInterceptor authorizationRelay() {
+        return tpl -> {
+            var attrs = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+            if (attrs != null) {
+                var auth = attrs.getRequest().getHeader(HttpHeaders.AUTHORIZATION);
+                if (StringUtils.hasText(auth)) {
+                    tpl.header(HttpHeaders.AUTHORIZATION, auth);
+                }
+            }
+        };
+    }
+
 }
 
 
